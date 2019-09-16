213f79973a80a36a89a29643195c64f685a00a9a
Rain <2516435583@qq.com>
2019-09-16 17:36:47 +0800

    Update util_test.go

--- util/util_test.go
+++ util/util_test.go
@@ -1,9 +1,13 @@
 package util
 
-
 import (
+	"fmt"
+	"math"
 	"testing"
+	"time"
 )
+
+
 
 func TestMaxTimeOfCI(t *testing.T) {
 	t.Log("begin")
@@ -12,5 +16,9 @@ func TestMaxTimeOfCI(t *testing.T) {
 		time.Sleep(time.Second*1)
 		t.Log("the ",i," second")
 	}
- 
+
+	fmt.Println("hello world 2")
+	if HelloWorld() != "hello world" {
+		t.Errorf("got %s expected %s", HelloWorld(), "hello world")
+	}
 }
