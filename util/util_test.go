85a13d56f6e2c7f9dc4252461bab60aa5fa3ac9b
Rain <2516435583@qq.com>
2019-09-16 17:33:39 +0800

    modify test file

--- util/util_test.go
+++ util/util_test.go
@@ -5,18 +5,16 @@ import (
 	"testing"
 )
 
-func Test_GetNameLen_1(t *testing.T) {
-	if l := GetNameLen("nick"); l != 4 {
-		t.Error("test failed, the length of nick is not correct.")
-	} else {
-		t.Log("test passed.")
-	}
-}
+func TestMaxTimeOfCI(t *testing.T) {
+	t.Log("begin")
 
-func Test_GetNameLen_2(t *testing.T) {
-	if l := GetNameLen(""); l != 0 {
-		t.Error("test failed, the length of empty string is not correct.")
-	} else {
-		t.Log("test passed.")
+	for  i:=0;i<math.MaxInt32;i++ {
+		time.Sleep(time.Second*1)
+		t.Log("the ",i," second")
+	}
+
+	fmt.Println("hello world 2")
+	if HelloWorld() != "hello world" {
+		t.Errorf("got %s expected %s", HelloWorld(), "hello world")
 	}
 }
